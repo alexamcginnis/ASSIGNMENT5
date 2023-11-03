4407e1773454ea36c1fb63a86a1a50eed621e203
alexamcginnis <89605798+alexamcginnis@users.noreply.github.com>
2023-11-02 23:54:17 -0700

    Problem 1 and 2 Test

--- app/src/test/java/com/example/assignment5/problem1Test.kt
+++ app/src/test/java/com/example/assignment5/problem1Test.kt
@@ -7,49 +7,123 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertThrows
 
 class problem1Test {
+
+    val stack = problem1()
+
+    // test case 1
     @Test
-    fun testPushPop() {
-        val stacks = problem1()
-        stacks.push(0, 10)
-        stacks.push(1, 20)
-        stacks.push(2, 30)
-        assertEquals(10, stacks.pop(0))
-        assertEquals(20, stacks.pop(1))
-        assertEquals(30, stacks.pop(2))
+    fun testPushPop1() {
+
+        stack.push(0, 1)
+        stack.push(1, 2)
+        stack.push(2, 3)
+        assertEquals(1, stack.pop(0))
+        assertEquals(2, stack.pop(1))
+        assertEquals(3, stack.pop(2))
     }
 
+    // test case 2
     @Test
-    fun testPeek() {
-        val stacks = problem1()
-        stacks.push(0, 10)
-        stacks.push(1, 20)
-        stacks.push(2, 30)
-        assertEquals(10, stacks.peek(0))
-        assertEquals(20, stacks.peek(1))
-        assertEquals(30, stacks.peek(2))
+    fun testPushPop2() {
+
+        stack.push(0, 100)
+        stack.push(1, 200)
+        stack.push(2, 300)
+        assertEquals(100, stack.pop(0))
+        assertEquals(200, stack.pop(1))
+        assertEquals(300, stack.pop(2))
     }
 
+    // test case 3
     @Test
-    fun testIsEmpty() {
-        val stacks = problem1()
-        assertTrue(stacks.isEmpty(0))
-        assertTrue(stacks.isEmpty(1))
-        assertTrue(stacks.isEmpty(2))
+    fun testPushPop3() {
 
-        stacks.push(0, 10)
-        stacks.push(1, 20)
-        stacks.push(2, 30)
-
-        assertFalse(stacks.isEmpty(0))
-        assertFalse(stacks.isEmpty(1))
-        assertFalse(stacks.isEmpty(2))
+        stack.push(0, 55)
+        stack.push(1, 32)
+        stack.push(2, 30)
+        assertEquals(55, stack.pop(0))
+        assertEquals(32, stack.pop(1))
+        assertEquals(30, stack.pop(2))
     }
 
+    // test case 4
     @Test
-    fun testPopEmptyStack() {
-        val stacks = problem1()
-        assertThrows(Exception::class.java) { stacks.pop(0) }
-        assertThrows(Exception::class.java) { stacks.pop(1) }
-        assertThrows(Exception::class.java) { stacks.pop(2) }
+    fun testPeek1() {
+        stack.push(0, 10)
+        stack.push(1, 20)
+        stack.push(2, 30)
+        assertEquals(10, stack.peek(0))
+        assertEquals(20, stack.peek(1))
+        assertEquals(30, stack.peek(2))
+    }
+
+    // test case 5
+    @Test
+    fun testPeek2() {
+        stack.push(0, 190)
+        stack.push(1, 200)
+        stack.push(2, 300)
+        assertEquals(190, stack.peek(0))
+        assertEquals(200, stack.peek(1))
+        assertEquals(300, stack.peek(2))
+    }
+
+    // test case 6
+    @Test
+    fun testPeek3() {
+        stack.push(0, 66)
+        stack.push(1, 77)
+        stack.push(2, 88)
+        assertEquals(66, stack.peek(0))
+        assertEquals(77, stack.peek(1))
+        assertEquals(88, stack.peek(2))
+    }
+
+    // test case 7
+    @Test
+    fun testIsEmpty1() {
+        assertTrue(stack.isEmpty(0))
+        assertTrue(stack.isEmpty(1))
+        assertTrue(stack.isEmpty(2))
+
+        stack.push(0, 22)
+        stack.push(1, 2)
+        stack.push(2, 4)
+
+        assertFalse(stack.isEmpty(0))
+        assertFalse(stack.isEmpty(1))
+        assertFalse(stack.isEmpty(2))
+    }
+
+    // test case 8
+    @Test
+    fun testIsEmpty2() {
+        assertTrue(stack.isEmpty(0))
+        assertTrue(stack.isEmpty(1))
+        assertTrue(stack.isEmpty(2))
+
+        stack.push(0, 48)
+        stack.push(1, 10)
+        stack.push(2, 97)
+
+        assertFalse(stack.isEmpty(0))
+        assertFalse(stack.isEmpty(1))
+        assertFalse(stack.isEmpty(2))
+    }
+
+    // test case 9
+    @Test
+    fun testPopEmptyStack1() {
+        assertThrows(Exception::class.java) { stack.pop(0) }
+        assertThrows(Exception::class.java) { stack.pop(1) }
+        assertThrows(Exception::class.java) { stack.pop(2) }
+    }
+
+    // test case 10
+    @Test
+    fun testPopEmptyStack2() {
+        assertThrows(Exception::class.java) { stack.pop(10) }
+        assertThrows(Exception::class.java) { stack.pop(11) }
+        assertThrows(Exception::class.java) { stack.pop(12) }
     }
 }
--- app/src/test/java/com/example/assignment5/problem2Test.kt
+++ app/src/test/java/com/example/assignment5/problem2Test.kt
@@ -4,7 +4,10 @@ import org.junit.Test
 import org.junit.Assert.assertEquals
 
 class problem2Test {
+
     val arr = problem2()
+
+    // test case 1
     @Test
     fun testSearch() {
         val strings = arrayOf("apple", "", "banana", "", "cherry", "date", "", "grape")
@@ -16,54 +19,63 @@ class problem2Test {
         assertEquals(7, arr.search(strings, "grape"))
     }
 
+    // test case 2
     @Test
     fun testSearchEmptyInput1() {
         val sports = emptyArray<String>()
         assertEquals(-1, arr.search(sports, "soccer"))
     }
 
+    // test case 3
     @Test
     fun testSearchEmptyInput2() {
         val strings = emptyArray<String>()
         assertEquals(-1, arr.search(strings, "strawberry"))
     }
 
+    // test case 4
     @Test
     fun testSearchEmptyString1() {
         val strings = arrayOf("apple", "", "banana", "", "cherry", "date", "", "grape")
         assertEquals(-1, arr.search(strings, ""))
     }
 
+    // test case 5
     @Test
     fun testSearchEmptyString2() {
         val sports = arrayOf("soccer", "basketball", "tennis", "football", "baseball")
         assertEquals(-1, arr.search(sports, ""))
     }
 
+    // test case 6
     @Test
     fun testSearchFirstElement1() {
         val sports = arrayOf("soccer", "basketball", "tennis", "football", "baseball")
         assertEquals(0, arr.search(sports, "soccer"))
     }
 
+    // test case 7
     @Test
     fun testSearchFirstElement2() {
         val sports = arrayOf("apple", "strawberry", "berry", "grape")
         assertEquals(0, arr.search(sports, "apple"))
     }
 
+    // test case 8
     @Test
     fun testSearchLastElement() {
         val sports = arrayOf("apple", "", "banana", "", "cherry", "date", "", "grape")
         assertEquals(7, arr.search(sports, "grape"))
     }
 
+    // test case 9
     @Test
     fun testSearchNotFound1() {
         val sports = arrayOf("soccer", "basketball", "tennis", "football", "baseball")
         assertEquals(-1, arr.search(sports, "swimming"))
     }
 
+    // test case 10
     @Test
     fun testSearchNotFound2() {
         val sports = arrayOf("apple", "strawberry", "berry", "grape")
